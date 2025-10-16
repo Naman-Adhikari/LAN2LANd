0a5f41bd8879792e712ebed77aa0ec75a4642400
Naman-Adhikari <adhikarinaman00@gmail.com>
2025-10-16 20:51:44 +0545

    main functin

--- LAN2LANd/src/main.rs
+++ LAN2LANd/src/main.rs
@@ -5,13 +5,18 @@ use std::path::Path;
 use std::thread;
 
 fn main() {
-println!("Simple LAN-2-LAN transfer");
-println!("1. Send file ");
-println!("1. Recieve file ");
-println!("Choose 1 or 2!");
-io::stdout().flush().unwrap();
+    println!("Simple LAN File Transfer");
+    println!("1. Send file");
+    println!("2. Receive file");
+    print!("Choose (1/2): ");
+    io::stdout().flush().unwrap();
 
-let mut choice = String::new();
-io::stdin().read_line(&mut choice).unwrap();
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).unwrap();
 
- 
+    match choice.trim() {
+        "1" => send_mode(),
+        "2" => receive_mode(),
+        _ => println!("Invalid choice"),
+    }
+}
