a120682f0d73a4c7ea2f1e09617f60739e4dd503
Naman-Adhikari <adhikarinaman00@gmail.com>
2025-10-17 01:56:16 +0545

    broken listener

--- LAN2LANd/src/main.rs
+++ LAN2LANd/src/main.rs
@@ -1,10 +1,12 @@
+use hostname;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::{self, Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, UdpSocket};
 use std::path::Path;
 use std::process::Command;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     println!("Simple LAN File Transfer");
@@ -24,6 +26,30 @@ fn main() {
 }
 
 fn send_mode() {
+    println!("Scanning for peers in your LAN....");
+
+    // Discover peers via UDP
+    let peers = discover_peers();
+    if peers.is_empty() {
+        println!("No peers found!");
+        return;
+    }
+
+    // Show peers if available
+    println!("Available Peers: ");
+    for (i, (name, ip)) in peers.iter().enumerate() {
+        println!("[{}] {} ({})", i + 1, name, ip);
+    }
+
+    print!("Select peer to send file: ");
+    io::stdout().flush().unwrap();
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).unwrap();
+
+    let choice: usize = choice.trim().parse().unwrap();
+
+    let (_, peer_ip) = &peers[choice - 1];
+
     let file_path = match Command::new("zenity")
         .arg("--file-selection")
         .arg("--title=Select a file to send")
@@ -39,13 +65,7 @@ fn send_mode() {
     };
     println!("selected file: {}", file_path);
 
-    println!("Enter receiver IP : 192.168.1.");
-    io::stdout().flush().unwrap();
-    let mut ip = String::new();
-    io::stdin().read_line(&mut ip).unwrap();
-    let ip = ip.trim();
-
-    match TcpStream::connect(format!("192.168.1.{}:7878", ip)) {
+    match TcpStream::connect(format!("{}:7878", peer_ip)) {
         Ok(mut stream) => {
             let path_obj = Path::new(&file_path);
             let file_name = path_obj.file_name().unwrap().to_string_lossy();
@@ -55,7 +75,6 @@ fn send_mode() {
                 .write_all(format!("{}\n", file_name).as_bytes())
                 .unwrap();
 
-            // send file bytes
             let mut buffer = [0u8; 4096];
             loop {
                 let n = file.read(&mut buffer).unwrap();
@@ -71,7 +90,6 @@ fn send_mode() {
 }
 
 fn receive_mode() {
-
     thread::spawn(|| {
         listen_for_discovery();
     });
@@ -119,9 +137,13 @@ fn discover_peers() -> Vec<(String, String)> {
     local_socket.set_broadcast(true).unwrap();
 
     let msg = "DISCOVER";
-    local_socket.send_to(msg.as_bytes(), broadcast_addr).unwrap();
+    local_socket
+        .send_to(msg.as_bytes(), broadcast_addr)
+        .unwrap();
 
-    local_socket.set_read_timeout(Some(Duration::from_secs(2))).unwrap();
+    local_socket
+        .set_read_timeout(Some(Duration::from_secs(2)))
+        .unwrap();
 
     let mut peers = Vec::new();
     let mut buf = [0u8; 128];
@@ -134,7 +156,6 @@ fn discover_peers() -> Vec<(String, String)> {
     }
     peers
 }
-
 
 fn listen_for_discovery() {
     let socket = UdpSocket::bind("0.0.0.0:9999").expect("Could not bind UDP discovery");
